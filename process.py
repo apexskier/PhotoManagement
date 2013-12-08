3563a8994eb68275035583a172e5596900d4bd8b
Cameron Little <cameron@camlittle.com>
2013-12-08 13:54:50 -0800

    changed some group names and folder naming

--- process.py
+++ process.py
@@ -36,24 +36,27 @@ for root, dirs, filenames in os.walk(import_folder):
             exif = get_exif_data(f_full)
             try:
                 photo_date = datetime.strptime(exif['DateTimeOriginal'], "%Y:%m:%d %H:%M:%S")
-                year_dir = str(photo_date.year)
-                month_dir = year_dir + '/' + photo_date.strftime('%B')
+                year_dir = os.path.join(export_folder, str(photo_date.year))
+                month_dir = os.path.join(year_dir, str(photo_date.month) + '-' + photo_date.strftime('%B'))
 
                 if not os.path.exists(year_dir):
                     os.makedirs(year_dir)
                 if not os.path.exists(month_dir):
                     os.makedirs(month_dir)
 
-                os.rename(f_full, month_dir + '/' + f)
+                os.rename(f_full, os.path.join(month_dir, f))
 
                 if month_dir not in modified_folders:
                     modified_folders.append(month_dir)
             except KeyError:
                 print "no original time for " + f_full
 
+subdirs = [x[0] for x in os.walk(export_folder) if x[0].count('/') == 2]
+
 # organize folders that have new stuff
-for folder in modified_folders:
+for folder in subdirs:
     data = []
+    print folder
 
     # get data for each file
     for root, dirs, filenames in os.walk(folder):
@@ -64,7 +67,7 @@ for folder in modified_folders:
                 exif = get_exif_data(f_full)
                 photo_date = datetime.strptime(exif['DateTimeOriginal'], "%Y:%m:%d %H:%M:%S")
                 year_dir = str(photo_date.year)
-                month_dir = year_dir + '/' + photo_date.strftime('%B')
+                month_dir = year_dir + '/' + str(photo_date.month) + '-' + photo_date.strftime('%B')
                 gpsinfo = {}
                 if 'GPSInfo' in exif:
                     for key, val in exif['GPSInfo'].iteritems():
@@ -78,7 +81,7 @@ for folder in modified_folders:
                     'gps': gpsinfo
                 })
 
-    data2 = []
+    groups = []
     last_dt = None
     dcount = 0
     start = 0
@@ -103,12 +106,14 @@ for folder in modified_folders:
         i += 1
     # save groups with more than 5 photos
     if dcount > 5:
-        data2.append((start, start + dcount))
+        groups.append((start, start + dcount))
+
+    print groups
 
     # move photos in to folders
     event_ct = 1
     event_names = []
-    for pair in data2:
+    for pair in groups:
         event_name = str(event_ct)
         avg = (pair[0] + pair[1]) / 2
 
@@ -137,7 +142,7 @@ for folder in modified_folders:
                 if ', USA' in address:
                     address = address.replace(', USA', '')
 
-                event_name = address
+                event_name = str(data[pair[0]]['date'].day) + '-' + address
                 if event_name in event_names:
                     event_name += ' ' + str(event_ct)
                 break
